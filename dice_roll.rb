8448d5dc7aed6e33691eb191e6dd721b43974fa8
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-03 23:04:44 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,11 +2,11 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-# def roll
-#   rand(1..6)
-# end
-
 def roll
-  dice = [1, 2, 3,  4, 5, 6]
-  dice.sample
+  rand(1..6)
 end
+
+# def roll
+#   dice = [1, 2, 3,  4, 5, 6]
+#   dice.sample
+# end
